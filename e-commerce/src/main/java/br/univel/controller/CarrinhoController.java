22ee4ab607db4d3fb01d3f245c8c16d294e94213
Felipe Mikael <Felipe Mikael@felipe>
2015-11-18 20:10:41 -0200

    Classe que irá realizar o controle do carrinho

---
+++ e-commerce/src/main/java/br/univel/controller/CarrinhoController.java
@@ -0,0 +1,33 @@
+package br.univel.controller;
+
+import javax.inject.Inject;
+import javax.ws.rs.PathParam;
+
+import br.univel.model.Produto;
+import br.univel.rest.ProdutoEndpoint;
+import br.univel.rest.VendaEndpoint;
+
+public class CarrinhoController {
+
+	@Inject
+	private Carrinho carrinho;
+
+	@Inject
+	private ProdutoEndpoint pe;
+
+	@Inject
+	private VendaEndpoint vendaEp;
+
+	public void adicionarProduto(@PathParam("id") long id) {
+		Produto p = pe.findById(id).readEntity(Produto.class);
+	}
+
+	public void limpar() {
+		carrinho.limpar();
+	}
+
+	public void finalizar() {
+
+	}
+
+}
